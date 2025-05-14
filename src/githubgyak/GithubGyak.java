c61231f89203e1de9cee3a1ff94bb8d666918c31
Domawas <dominknagy@gmail.com>
2025-05-14 08:12:27 +0200

    GitHub osszekotese

---
+++ src/githubgyak/GithubGyak.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githubgyak;
+
+/**
+ *
+ * @author NagyDominik(Szf_N_20
+ */
+public class GithubGyak {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int szam = 10;
+        for (int i = 1; i <= szam; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
